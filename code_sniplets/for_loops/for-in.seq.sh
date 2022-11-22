d807b4e386ccec753771304ccb074c4331506d34
davidh <mriservice@gmail.com>
2022-11-22 11:26:19 -0500

    Added for loops

---
+++ code_sniplets/for_loops/for-in.seq.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This is a Comment
+# echo "Hello Dude" > test.txt
+
+for i in $(seq 10)
+	do
+		echo $(seq 10)
+	done
---
+++ code_sniplets/for_loops/for2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This is a Comment
+# echo "Hello Dude" > test.txt
+
+for (( i=1; i<=10; i++ ))
+	do
+		echo $(seq 10)
+	done
